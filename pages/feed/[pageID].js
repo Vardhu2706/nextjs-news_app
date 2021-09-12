e9aaa2c242ec371bba960cd6c10501c73d2ec9de
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-09-13 00:03:00 +0530

    Displated artciles and added pagination.

--- pages/feed/[pageID].js
+++ pages/feed/[pageID].js
@@ -1,18 +1,77 @@
+import router from "next/router";
 import Toolbar from "../../components/toolbar";
-import styles from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
+import styles from "../../styles/Feed.module.css";
 
 export const Feed = ({ pageNumber, articles }) => {
-  console.log(articles, pageNumber);
+  const router = useRouter();
+  console.log(articles);
   return (
-    <div className="page-container">
-      {/* Toolbar */}
-      <Toolbar />
+    <>
+      <div className="page-container">
+        {/* Toolbar */}
+        <Toolbar />
 
-      {/* Home Page Content */}
-      <div className={styles.main}>
-        <h1>Headlines</h1>
+        {/* Home Page Content */}
+        <div className={styles.main}>
+          {articles.map((article, index) => {
+            if (
+              article.title &&
+              article.description &&
+              article.urlToImage &&
+              article.url
+            ) {
+              return (
+                <div key={index} className={styles.post}>
+                  <h1 onClick={() => (window.location.href = article.url)}>
+                    {article.title}
+                  </h1>
+                  <p>{article.description}</p>
+                  {!!article.urlToImage && (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img src={article.urlToImage} alt="news-image" />
+                  )}
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className={styles.paginator}>
+          <div
+            className={pageNumber === 1 ? styles.disabled : styles.active}
+            onClick={() => {
+              if (pageNumber > 1) {
+                router.push(`/feed/${pageNumber - 1}`);
+              }
+            }}
+          >
+            Previous Page
+          </div>
+
+          <div>#{pageNumber}</div>
+
+          <div
+            className={pageNumber === 5 ? styles.disabled : styles.active}
+            onClick={() => {
+              if (pageNumber < 5) {
+                // As of the current version of Next.js the default behavior for router.push
+                // will leave the scroll where it is, so we have to manually call scrollTo.
+                // This however is being worked on and is fixed in canary.
+                // Show this in tutorial vid:
+                // https://github.com/vercel/next.js/issues/3249
+                router
+                  .push(`/feed/${pageNumber + 1}`)
+                  .then(() => window.scrollTo(0, 0));
+              }
+            }}
+          >
+            Next Page
+          </div>
+        </div>
       </div>
-    </div>
+
+      {/* Paginator */}
+    </>
   );
 };
 
@@ -32,7 +91,7 @@ export const getServerSideProps = async (pageContext) => {
 
   // Fetching Data
   const apiResponse = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=in&pageSize=5&page=${pageNumber}`,
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=8&page=${pageNumber}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.NEWS_API}`,
